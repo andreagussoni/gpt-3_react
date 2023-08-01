ddcc5cea57260b2cf9617e92bbfb4303bde445d1
Andrea Gussoni <andreagus@outlook.it>
2023-08-01 17:09:38 +0200

    new sections

--- src/components/cta/Cta.jsx
+++ src/components/cta/Cta.jsx
@@ -3,7 +3,15 @@ import'./cta.css'
 
 function Cta() {
   return (
-    <div>Cta</div>
+    <div className='gpt3__cta'>
+      <div className="gpt3__cta-content">
+        <p>Request early access</p>
+        <h3>Register today and start exploring</h3>
+      </div>
+      <div className="gpt3__cta-btn">
+        <button type='button'>Get started</button>
+      </div>
+    </div>
   )
 }
 
--- src/containers/possibility/Possibility.jsx
+++ src/containers/possibility/Possibility.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import './possibility.css'
+import possibilityImage from '../../assets/possibility.png'
 
 function Possibility() {
   return (
-    <div>Possibility</div>
+    <div className='gpt3__possibility section__padding' id='possibility'>
+      <div className="gpt3__possibility-image">
+        <img src={possibilityImage} alt="possibility" />
+      </div>
+      <div className="gpt3__possibility-content">
+        <h4>Request early access</h4>
+        <h1 className="gradient__text">The possibilities are beyonnd imagination</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit laboriosam tempora consectetur sunt ratione earum quam dolore, iste suscipit illo sequi ex alias nisi obcaecati ipsa, adipisci culpa repudiandae non!</p>
+        <h4>Request early access</h4>
+      </div>
+    </div>
   )
 }
 
